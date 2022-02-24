d4d9ff169e3e40f1f7e3229259cf549c97e57d91
Mariel Hoepelman <marielhoepelman@gmail.com>
2022-02-24 13:48:30 -0500

    Async changes

--- assets/app.js
+++ assets/app.js
@@ -1,48 +1,25 @@
-(() => {
-  const quotes = [
-    {
-      quote:
-        "Life is too short and sweet to be spent by cribbing and complaining about things. Here are some random quotes about the most wonderful gift that we've got",
-      author: " Life"
-    },
-    {
-      quote:
-        "Humor is richly rewarding to the person who employs it. It has some value in gaining and holding attention. But it has no persuasive value at all",
-      author: "John Kenneth Galbraith"
-    },
-    {
-      quote:
-        "God save me from my friends. I can protect myself from my enemies.",
-      author: "Claude Louis Hector de Villars "
-    },
-    {
-      quote: "The price of anything is the amount of life you exchange for it.",
-      author: "David Thoreau"
-    },
-    {
-      quote:
-        "Life is like a landscape. You live in the midst of it but can describe it only from the vantage point of distance. ",
-      author: "Charles Lindbergh"
-    },
-    {
-      quote:
-        "A critic is someone who never actually goes to the battle, yet who afterwards comes out shooting the wounded.",
-      author: " Tyne Daly"
-    }
-  ];
 
-  /* Add JS here */
-  const btn = document.getElementById("btn");
-
-  const randomQuoteGenerator = () => {
-    const quoteElement = document.getElementById("quote");
-    const authorElement = document.getElementById("author");
-    const index = Math.floor(Math.random() * quotes.length);
-    const randomQuote = quotes[index];
-    quoteElement.innerHTML = randomQuote.quote;
-    authorElement.innerHTML = randomQuote.author;
+const fetchAsyncQuotes = async () => {
+  try {
+    const response = await fetch("https://type.fit/api/quotes")
+    return await response.json();
+  } catch (error) {
+    console.log(error)
   }
+}
+const quotes = fetchAsyncQuotes(); // returns a promise that need to be either thened or awaited
+const btn = document.getElementById("btn");
+const quoteElement = document.getElementById("quote");
+const authorElement = document.getElementById("author");
 
-  window.addEventListener('load', randomQuoteGenerator);
-  btn.addEventListener('click', randomQuoteGenerator);
-})();
+const randomQuoteGenerator = async () => {
+  const result = await quotes;
+  const index = Math.floor(Math.random() * result.length);
+  const randomQuote = result[index];
+  quoteElement.innerHTML = randomQuote.text;
+  authorElement.innerHTML = randomQuote.author;
+}
+
+document.addEventListener('DOMContentLoaded', randomQuoteGenerator);
+btn.addEventListener('click', randomQuoteGenerator);
+
